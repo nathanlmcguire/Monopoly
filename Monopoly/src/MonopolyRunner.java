af9161f596fc13b7fb11650538b292ff2d4a1f73
natha <natha@LAPTOP-NBO8904H>
2020-02-08 11:25:01 -0700

    Added a more humble comment

--- Monopoly/src/MonopolyRunner.java
+++ Monopoly/src/MonopolyRunner.java
@@ -6,6 +6,8 @@ public class MonopolyRunner
 	public static void main(String[] args) throws IOException
 		{
 		//THE ONE TRUE MCGUIRE
+		//Still needs a little help
+		int uselessVariable = 0;
 		Square.chooseTheme();
 		Square.readData();
 		while (Player.p.getMoney() > 0)
